38babd9bf8b195d5f42de29e77c8a546b20ba644
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-27 08:43:15 +0100

    Add project files.

---
+++ Ch08Ex01/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Ch08Ex01
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
